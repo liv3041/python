d947401b7fa1e1b51110de99498df5ac4b0da2c0
Anjali <veranjali2503@gmail.com>
2025-03-22 14:58:50 +0530

    error and exception

---
+++ basics/codes/errorsAndExceptions.py
@@ -0,0 +1,11 @@
+"""
+Errors:
+There are two types of errors: 
+1. syntax errors
+2. exceptions
+
+Syntax Errors:
+- Is also known as parsing errors.
+"""
+# In the below statement syntax error would be if there is no colon after true.
+while True: print("Hello world")
